0100453502f3b9ac5775876243e3d320962f06d0
valdenio458 <valdeniomartinho@gmail.com>
2022-07-05 18:15:16 -0300

    Dica Pythônica 16

---
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/dica_pythonica16.py
@@ -0,0 +1,12 @@
+def produto(num, other_num=1):
+    return num * other_num
+
+# parâmetros posiiconais
+# produto(num, other_num)
+
+
+print(produto(2, 2))
+print(produto(2))
+
+# nomeados
+print(produto(other_num=7, num=10))
