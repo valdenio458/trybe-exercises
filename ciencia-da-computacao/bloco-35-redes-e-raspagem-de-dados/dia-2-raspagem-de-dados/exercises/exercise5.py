e421372f8c703be8e9d6098e203131591eab6c1f
valdenio458 <valdeniomartinho@gmail.com>
2022-07-19 15:47:10 -0300

    Exercício 5

---
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercises/exercise5.py
@@ -0,0 +1,29 @@
+import requests
+import parsel
+
+# Título h1::text
+# Preço .product_main > .price_color::text
+# Descrição #product_description ~ p::text
+# URL #product_gallery img::attr(src)
+# Disponibilidade .product_main .availability::text
+
+URL_BASE = 'http://books.toscrape.com/catalogue/'
+response = requests.get(URL_BASE + 'the-grand-design_405/index.html')
+selector = parsel.Selector(response.text)
+
+title = selector.css("h1::text").get()
+
+price = selector.css(".product_main > .price_color::text").re_first(r'\d*\.\d{2}')
+
+description = selector.css("#product_description ~ p::text").get()
+sufix = '...more'
+
+if description.endswith(sufix):
+    description = description[:-len(sufix)] # pega tudo menos o sufixo
+
+cover = URL_BASE + selector.css("#product_gallery img::attr(src)").get()
+
+availability = selector.css(".product_main .availability::text").re_first(r'\d')
+
+
+print(title, price, description, cover, availability, sep=',')
