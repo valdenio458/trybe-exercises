f57baebf875ab4c3a56a4ebce1ed47881abd575a
valdenio458 <valdeniomartinho@gmail.com>
2022-07-25 10:45:03 -0300

    Exercício 1 - Adiciona comentário

--- ciencia-da-computacao/bloco-36-algoritmos/dia-1-complexidade-de-algoritmos/exercises/exercise1.py
+++ ciencia-da-computacao/bloco-36-algoritmos/dia-1-complexidade-de-algoritmos/exercises/exercise1.py
@@ -1,4 +1,6 @@
-def contains_duplictae(numbers):
+# Dado um array de números de tamanho n, escreva um algoritmo que retorna True se houver no array um número duplicado e False caso contrário. Analise a solução abaixo e diga qual é a ordem de complexidade desse algoritmo para o melhor caso, pior caso e caso médio.
+
+def contains_duplicate(numbers):
   numbers.sort()
   previous_number = ''
   for number in numbers:
@@ -7,4 +9,4 @@ def contains_duplictae(numbers):
     previous_number = number
   return False
 
-print(contains_duplictae([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+print(contains_duplicate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
