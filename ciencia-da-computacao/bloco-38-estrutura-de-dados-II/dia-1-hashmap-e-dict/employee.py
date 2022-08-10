9ca40d7f6c909017927a101b7cfbaabc7d8582e4
valdenio458 <valdeniomartinho@gmail.com>
2022-08-09 22:21:54 -0300

    Classe employee

---
+++ ciencia-da-computacao/bloco-38-estrutura-de-dados-II/dia-1-hashmap-e-dict/employee.py
@@ -0,0 +1,16 @@
+class Employee:
+    def __init__(self, id_num, name):
+        self.id_num = id_num
+        self.name = name
+
+
+class HashMap:
+    def __init__(self):
+        self._buckets = [None for _ in range(10)]
+
+    def get_address(self, id_num):
+        return id_num % 10
+
+    def insert(self, employee):
+        address = self.get_address(employee.id_num)
+        self._buckets[address] = employee
